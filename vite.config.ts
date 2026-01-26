9b6594ff6bb4f1bdce661efc43d3095650761b4d
Pon157 <Vitechek.208@gmail.com>
2026-01-26 20:12:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  define: {
+    // Обеспечивает доступность process.env в клиентском коде
+    'process.env': process.env
+  },
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   },
 });
