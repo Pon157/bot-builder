ad27703005b9da73a6e492947bb74d8a294062f4
Pon157 <Vitechek.208@gmail.com>
2026-02-18 21:11:51 +0300

    Update bot_core.py

--- bot_core.py
+++ bot_core.py
@@ -788,7 +788,7 @@ class BotInstance:
             )
             return True
 
-        async def admin_control_logic(self, m: Message, command: str):
+    async def admin_control_logic(self, m: Message, command: str):
         """
         ПОЛНАЯ ЛОГИКА АДМИН-КОМАНД (БАН, ВАРН, РАЗБАН)
         Включает поиск пользователя и немедленную синхронизацию с БД.
