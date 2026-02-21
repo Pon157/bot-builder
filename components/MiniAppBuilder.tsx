563fea63a3846f135ff96aba6c38149b66a88ec3
Pon157 <Vitechek.208@gmail.com>
2026-02-21 10:45:02 +0300

    Update MiniAppBuilder.tsx

--- components/MiniAppBuilder.tsx
+++ components/MiniAppBuilder.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 import {
   Plus, Trash2, ChevronUp, ChevronDown, Settings2, Eye, EyeOff,
   Type, MousePointerClick, Link2, TextCursorInput, AlignLeft,
