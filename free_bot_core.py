16752e17e5dda54fff1763c8d4e76fe18833c7ad
Pon157 <Vitechek.208@gmail.com>
2026-03-16 21:42:32 +0300

    Update free_bot_core.py

--- free_bot_core.py
+++ free_bot_core.py
@@ -40,7 +40,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)]
 )
